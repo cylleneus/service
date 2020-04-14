87eec395a592515560af103e04ed8ccb3d9276de
William Short <w.short@exeter.ac.uk>
2020-04-14 20:32:27 +0100

    update repo

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-version = "0.0.2"
+version = "0.0.3"
 
 requirements = ["cylleneus>=0.3.4", "celery[redis]>=4.4.0", "fastapi>=0.46.0", "rabbitmq>=0.2.0", "uvicorn>=0.11.1"]
 
