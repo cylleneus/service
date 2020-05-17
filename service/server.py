c214d8b233ddb71905d6963f1a225abb57940773
ws308 <w.short@exeter.ac.uk>
2020-05-17 20:03:34 +0100

    update requirements, formatting

--- service/server.py
+++ service/server.py
@@ -4,7 +4,14 @@ from .main import app
 
 
 def run():
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info", workers=4, reload=True)
+    uvicorn.run(
+        "main:app",
+        host="127.0.0.1",
+        port=8000,
+        log_level="info",
+        workers=4,
+        reload=True,
+    )
 
 
 if __name__ == "__main__":
